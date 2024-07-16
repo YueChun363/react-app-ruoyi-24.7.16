f3267f9306ed9eec4fe21b2e0883e0046b3d40cc
yc <1013582515@qq.com>
2024-07-16 13:35:30 +0800

    css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
